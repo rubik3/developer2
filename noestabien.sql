0d319a4528961f8ea5a68640a6de068c57190fde
juananamoriza <48628623+juananamoriza@users.noreply.github.com>
2019-04-08 22:54:32 +0200

    Create noestabien.sql

---
+++ noestabien.sql
@@ -0,0 +1,11 @@
+create table TABLAENDE
+(
+  num         number(5),
+  nom         varchar2(30),
+  dir         varchar2(50),
+  fecha_nac   date
+  CONSTRAINT cp_tablaende PRIMARY KEY (num)
+ )
+ /
+ 
+  
