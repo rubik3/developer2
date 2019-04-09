0f402af240b85151d259802881c6202163153570
Yeray <>
2019-04-09 10:14:25 +0200

    Error solucionado

--- noestabien.sql
+++ noestabien.sql
@@ -3,7 +3,7 @@ create table TABLAENDE
   num         number(5),
   nom         varchar2(30),
   dir         varchar2(50),
-  fecha_nac   date
+  fecha_nac   date,
   CONSTRAINT cp_tablaende PRIMARY KEY (num)
  )
  /
